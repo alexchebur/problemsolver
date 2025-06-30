49b9425abab1c71d00590ce7b87e67075e77511a
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-06-30 17:30:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ if not api_key:
     st.warning("Пожалуйста, введите API-ключ")
     st.stop()
 
+if st.secrets.get("network", {}).get("enableProxy", False):
+    os.environ["HTTP_PROXY"] = "http://proxy.streamlit.io:3128"
+    os.environ["HTTPS_PROXY"] = "http://proxy.streamlit.io:3128"
+
+
+
 genai.configure(
     api_key=api_key,
     transport='rest',
